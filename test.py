9eca00a11f013bd5ace82401b66de0c4b5566114
Kelly Netterville <36lima@gmail.com>
2023-08-04 15:55:09 -0500

    Removed unwanted file.

--- test.py
+++
@@ -1,7 +0,0 @@
-user = input("What is your username?: ")
-password = input("What is your password?:")
-obscured_pw = "*" * len(password)
-
-print(
-    f"User {user} has a password of {obscured_pw} that is {len(password)} characters long."
-)
